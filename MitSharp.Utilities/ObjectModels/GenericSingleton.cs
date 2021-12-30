ae53645815b0b91159a3625de229bbe8ce1c814f
Mitesh Shah <mitsha@microsoft.com>
2021-12-30 15:07:16 +0530

    Added Generic Singleton

--- MitSharp.Utilities/ObjectModels/GenericSingleton.cs
+++ MitSharp.Utilities/ObjectModels/GenericSingleton.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MitSharp.Utilities.ObjectModels
+﻿namespace MitSharp.Utilities.ObjectModels
 {
-    internal class GenericSingleton
+    /// <summary>
+    /// Generic testable singleton class for concrete type <typeparamref name="TCon"/> implementing interface <typeparamref name="TInt"/>.
+    /// </summary>
+    /// <typeparam name="TInt">Interface Type.</typeparam>
+    /// <typeparam name="TCon">Concrete Type for given interface type.</typeparam>
+    public class GenericSingleton<TInt, TCon>
+        where TCon : TInt, new()
     {
+        private static TInt _instance;
+
+        public static void SetInstance(TInt newInstance)
+        {
+            _instance = newInstance;
+        }
+
+        public static TInt Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new TCon();
+                }
+
+                return _instance;
+            }
+        }
     }
 }
